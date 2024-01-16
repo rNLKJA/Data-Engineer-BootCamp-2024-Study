7bacc223aeffa007658e1cd69447498e9b14e726
rNLKJA <rNLKJA@non-use.com>
2024-01-16 22:07:06 +1100

    completed video 2

---
+++ week1_introduction_and_prerequisites/pgcli.sh
@@ -0,0 +1,1 @@
+pgcli -h localhost -p 5432 -U root -d ny_taxi
---
+++ week1_introduction_and_prerequisites/postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+docker run -d -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  postgres:13
+
+docker ps # display running containers
